2249e9922838b2f499bb94bf3d35db56a58d2876
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2021-09-09 18:53:00 +0200

    Update projects layout for mobile

--- components/project/item.vue
+++ components/project/item.vue
@@ -2,7 +2,12 @@
   <v-card shaped>
     <v-img src="/images/aviatorjk_bark.jpg" height="196px" />
     <v-card-title>
-      <h3>{{ project.name }}</h3>
+      <h3 style="width: 100%">{{ project.name }}</h3>
+      <v-chip-group>
+        <v-chip v-for="(techno, index) in project.technos" :key="index">
+          {{ techno }}
+        </v-chip>
+      </v-chip-group>
     </v-card-title>
     <v-card-text>
       <ul>
--- components/project/list.vue
+++ components/project/list.vue
@@ -1,11 +1,12 @@
 <template>
   <v-container>
-    <h2 class="my-16">{{ $t("home__projects") }}</h2>
+    <h2 class="my-16 d-block text-truncate">{{ $t("home__projects") }}</h2>
     <v-row>
       <v-col
         v-for="(project, index) in projects"
         :key="index"
-        cols="5"
+        cols="12"
+        md="5"
         :offset-md="index % 2 === 0 ? '0' : '2'"
       >
         <project-item :project="project" />
@@ -21,6 +22,7 @@ export default {
       projects: [
         {
           name: "Ginko Real Time Widget",
+          technos: ["Android", "Kotlin", "SQLite", "XML"],
           bullets: [
             "Bullet point number 1",
             "Bullet point number 2",
@@ -31,6 +33,7 @@ export default {
         },
         {
           name: "Cavity",
+          technos: ["Android", "Java", "SQLite", "XML"],
           bullets: [
             "Bullet point number 1",
             "Bullet point number 2",
