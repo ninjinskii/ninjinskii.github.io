3f9bbace39e917e5f4f772537de78287492ba2af
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2021-09-09 23:09:11 +0200

    Working on home icons

--- components/top-header.vue
+++ components/top-header.vue
@@ -1,16 +1,39 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" md="8">
+      <v-col cols="8">
         <h1>Bailly Louis</h1>
         <p>Ma petite phrase</p>
       </v-col>
-      <v-col cols="12" md="4">
-        <ul>
-          <li>Github</li>
-          <li>CV</li>
-        </ul>
+      <v-col cols="4">
+        <v-row>
+          <v-col cols="6">
+            <v-btn class="d-block mx-auto" x-large icon>
+              <v-icon x-large>{{ mdiGithub }}</v-icon>
+            </v-btn>
+            <p class="text-center text-overline">{{ $t("home__github") }}</p>
+          </v-col>
+          <v-col cols="6">
+            <v-btn class="d-block mx-auto" x-large icon>
+              <v-icon x-large>{{ mdiFileOutline }}</v-icon>
+            </v-btn>
+            <p class="text-center text-overline">{{ $t("home__resume") }}</p>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+import { mdiGithub, mdiFileOutline } from "@mdi/js";
+
+export default {
+  data() {
+    return {
+      mdiGithub,
+      mdiFileOutline,
+    };
+  },
+};
+</script>
