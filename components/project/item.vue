3683b787d7e1d43684f4348ef2eac8491df9c8eb
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2021-09-13 18:04:00 +0200

    Rework project items

--- components/project/item.vue
+++ components/project/item.vue
@@ -1,44 +1,63 @@
 <template>
-  <v-card>
-    <v-img src="/images/aviatorjk_bark.jpg" height="196px" />
-    <v-card-title>
-      <h3 style="width: 100%">{{ project.name }}</h3>
-      <v-chip-group column>
-        <v-chip v-for="(techno, index) in project.technos" :key="index">
-          {{ techno }}
-        </v-chip>
-      </v-chip-group>
-    </v-card-title>
-    <v-card-text>
-      <ul>
-        <li v-for="(bullet, index) in project.bullets" :key="index">
-          {{ bullet }}
-        </li>
-      </ul>
-    </v-card-text>
-    <v-card-actions>
-      <v-spacer />
-      <v-btn outlined color="primary" class="mr-4"> Code </v-btn>
-      <v-btn
-        color="primary"
-        class="mr-4"
-        outlined
-        nuxt
-        :to="`/project/${project.id}`"
-      >
-        Demo
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+  <v-container fluid px-0>
+    <v-row>
+      <v-col cols="2">
+        <v-img
+          src="/images/aviatorjk_bark.jpg"
+          class="rounded-circle"
+          height="196px"
+          width="196px"
+        />
+      </v-col>
+      <v-col>
+        <h2 class="mb-n3 text-center text-md-left" style="width: 100%">
+          {{ project.name }}
+        </h2>
+        <div class="text-overline text--disabled text-center text-md-left">
+          Horaires en temps réel des bus de Besançon (25)
+        </div>
+        <v-chip-group column>
+          <v-chip
+            v-for="(techno, index) in project.technos"
+            :key="index"
+            :ripple="false"
+          >
+            {{ techno }}
+          </v-chip>
+        </v-chip-group>
+        <p class="text--medium text-justify text-sm-left">
+          Cette application permet d'enregistrer les trajets les plus fréquents
+          que l'utilisateur effectue pour ensuite lui afficher les temps de
+          passages des bus.
+        </p>
+        <v-btn class="mt-4" outlined>
+          {{ $t("show__code") }}
+          <v-icon right>
+            {{ mdiOpenInNew }}
+          </v-icon>
+        </v-btn>
+        <v-btn class="mt-4 ml-4" outlined>
+          {{ $t("show__demo") }}
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import { mdiOpenInNew } from "@mdi/js";
+
 export default {
   props: {
     project: {
       type: Object,
       default: () => ({}),
     },
+  },
+  data() {
+    return {
+      mdiOpenInNew,
+    };
   },
 };
 </script>
--- components/project/list.vue
+++ components/project/list.vue
@@ -2,14 +2,9 @@
   <v-container>
     <h2 class="my-16 d-block text-truncate">{{ $t("home__projects") }}</h2>
     <v-row>
-      <v-col
-        v-for="(project, index) in projects"
-        :key="index"
-        cols="12"
-        md="5"
-        :offset-md="index % 2 === 0 ? '0' : '2'"
-      >
+      <v-col v-for="(project, index) in projects" :key="index" cols="12">
         <project-item :project="project" />
+        <v-divider v-if="index !== projects.length - 1" class="mt-8" />
       </v-col>
     </v-row>
   </v-container>
